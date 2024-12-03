0a1ab5076c3780eb29fab042332ee46052681f7a
Yasmagalhaes <00001107577688sp@al.educacao.sp.gov.br>
2024-12-03 15:49:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Corinthians',
+    'Quando foi criado o Corinthians?',
+    'Dia 1 de Setembro de 1910'
+)
+
+criaCartao(
+    'Corinthians',
+    'Quando foi o ultimo Mundial do corinthians?',
+    'Foi em 2012, contra o Chelsea'
+)
+
+criaCartao(
+    'Corinthians',
+    'Quem é o altual camisa 10 e o camisa 9 ?',
+    'O atual camisa 10 é o Rodrigo Garro e o camisa 9 Yuri alberto'
+)
+
+criaCartao(
+    'Corinthians',
+    'Qual é o atual artilheiro da arena corinthians?',
+    'Angel Romero'
+)
